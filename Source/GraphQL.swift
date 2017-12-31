a17f7778c363d19e2d9113680bbbbacc5d1af0ae
Florian Mari <florian.mari@epitech.eu>
2017-12-31 19:07:05 +0100

    Add Subscription option

--- Source/GraphQL.swift
+++ Source/GraphQL.swift
@@ -28,114 +28,130 @@
 import Foundation
 
 class NonEscapedGraphQLValue {
-    
+
     let value: String
-    
+
     init(value: String) {
         self.value = value
     }
 }
 
 class GraphQL {
-    
-    private(set) var isMutation: Bool
-    private(set) var name: String?
-    private(set) var mutations: [ String : GraphQL ]
-    private(set) var params: [ String : AnyObject ]
-    private(set) var fields: [ AnyObject ]
-    
-    internal required init(name: String? = nil, isMutation: Bool = false, params: [ String : AnyObject ] = [ : ], fields: [ AnyObject ] = [], mutations: [ String : GraphQL ] = [ : ]) {
-        
-        self.isMutation = isMutation
-        self.name      = name
-        self.mutations = mutations
-        self.params    = params
-        self.fields    = fields
+
+    enum QueryType {
+        case query, mutation, subscription
     }
-    
+
+    private(set) var isMutation: Bool = false
+    private(set) var isSubscription: Bool = false
+    private(set) var name: String?
+    private(set) var mutations: [String: GraphQL]
+    private(set) var params: [String: AnyObject]
+    private(set) var fields: [AnyObject]
+
+    internal required init(name: String? = nil, queryType: QueryType = .query, params: [String: AnyObject] = [:], fields: [AnyObject] = [], mutations: [String: GraphQL] = [:]) {
+        switch queryType {
+        case .mutation:
+            isMutation = true
+        case .subscription:
+            isSubscription = true
+        default:
+            isMutation = false
+            isSubscription = false
+        }
+        self.name = name
+        self.mutations = mutations
+        self.params = params
+        self.fields = fields
+    }
+
     public func build(isSubquery: Bool) -> String {
-        
+
         var string = ""
-        
+
         if let name = self.name {
             string += name
         }
-        
-        if !self.mutations.isEmpty {
-            string += " { \(mapParams(params: self.mutations)) }"
+
+        if !mutations.isEmpty {
+            string += " { \(mapParams(params: mutations)) }"
         }
-        
-        if !self.params.isEmpty {
-            string += " ( \(mapParams(params: self.params)) )"
+
+        if !params.isEmpty {
+            string += " ( \(mapParams(params: params)) )"
         }
-        
-        if !self.fields.isEmpty {
-            string += " { \(mapFields(fields: self.fields)) }"
+
+        if !fields.isEmpty {
+            string += " { \(mapFields(fields: fields)) }"
         }
-        
-        if !isSubquery && self.name != nil {
+
+        if !isSubquery && name != nil {
             string = "{ \(string) }"
         }
-        
-        if !isSubquery && self.isMutation {
+
+        if !isSubquery && isMutation {
             string = "mutation \(string)"
         }
-        
+
+        if !isSubquery && isSubscription {
+            string = "subscription \(string)"
+        }
+
         return string
     }
-    
+
     // First methods
-    
+
     static func query(name: String? = nil) -> Self {
         return self.init(name: name)
     }
-    
-    static func queries(queries: [ String : GraphQL ]) -> Self {
+
+    static func queries(queries: [String: GraphQL]) -> Self {
         return self.init(mutations: queries)
     }
-    
+
     static func mutation(name: String? = nil) -> Self {
-        return self.init(name: name, isMutation: true)
+        return self.init(name: name, queryType: .mutation)
     }
-    
-    static func mutations(mutations: [ String : GraphQL ]) -> Self {
-        return self.init(isMutation: true, mutations: mutations)
+
+    static func mutations(mutations: [String: GraphQL]) -> Self {
+        return self.init(queryType: .mutation, mutations: mutations)
     }
-    
+
     // Name
-    
+
     func name(name: String) -> Self {
         self.name = name
         return self
     }
-    
+
     // Fields
-    
-    func fields(fields: [ AnyObject ]) -> Self {
+
+    func fields(fields: [AnyObject]) -> Self {
         self.fields = fields
         return self
     }
-    
+
     func field(field: AnyObject) -> Self {
-        self.fields.append(field)
+        fields.append(field)
         return self
     }
-    
+
     // Params
-    
+
     func params(key: String, value: AnyObject?) -> Self {
-        self.params[key] = value
+        params[key] = value
         return self
     }
-    
+
     func param(key: String, value: AnyObject?) -> Self {
-        self.params[key] = value
+        params[key] = value
         return self
     }
 }
 
 extension GraphQL: CustomStringConvertible {
-    
+
     var description: String {
         let desc = build(isSubquery: false)
         print("query desc = \(desc)")
@@ -144,69 +160,68 @@ extension GraphQL: CustomStringConvertible {
 }
 
 private func mapValue(value: AnyObject) -> String {
-    
+
     // STRING
     if let value = value as? NonEscapedGraphQLValue {
         return value.value
-        
-    } // STRING
-    else if let value = value as? String {
+
+    } else if let value = value as? String {
         return "\"\(value.replacingOccurrences(of: "\"", with: "\\\""))\""
-        
+
         // INT & BOOL
     } else if value is Int {
-        
-        // TODO : Find a better way to detect booleans
+
+        // TODO: Find a better way to detect booleans
         if "\(type(of: value))" == "__NSCFBoolean" {
             return (value as! Bool) ? "true" : "false"
         } else {
             return "\(value)"
         }
-        
+
         // FLOAT & DOUBLE
     } else if value is Float || value is Double {
         return "\(value)"
-        
+
         // ARRAY
     } else if let value = value as? Array<AnyObject> {
         let mappedSubvalues = value
-            .map({ subvalue in return mapValue(value: subvalue) })
+            .map({ subvalue in mapValue(value: subvalue) })
             .joined(separator: ", ")
-        
+
         return "[ \(mappedSubvalues) ]"
-        
+
         // DICTIONARY
     } else if let value = value as? Dictionary<String, AnyObject> {
         let mappedSubvalues = value
-            .map({ (key, subvalue) in return "\(key) : \(mapValue(value: subvalue))" })
+            .map({ key, subvalue in "\(key) : \(mapValue(value: subvalue))" })
             .joined(separator: ", ")
-        
+
         return "{ \(mappedSubvalues) }"
-        
+
     } else if let value = value as? GraphQL {
         return value.build(isSubquery: true)
-        
+
     } else {
         return "null"
     }
 }
 
-private func mapParams(params: [ String : AnyObject ]) -> String {
-    
+private func mapParams(params: [String: AnyObject]) -> String {
+
     return params
-        .map({ (key, value) in return "\(key) : \(mapValue(value: value))" })
+        .map({ key, value in "\(key) : \(mapValue(value: value))" })
         .joined(separator: ", ")
 }
 
-private func mapFields(fields: [ AnyObject ]) -> String {
-    
+private func mapFields(fields: [AnyObject]) -> String {
+
     return fields
         .map { field in
-            
+
             if let field = field as? GraphQL {
-                return  "\(field.build(isSubquery: true))"
+                return "\(field.build(isSubquery: true))"
             } else {
-                return  "\(field)"
+                return "\(field)"
             }
         }
         .joined(separator: " ")
